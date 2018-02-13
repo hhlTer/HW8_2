ed9697f93be238df68df49b762020655f38d45d0
hhlTer <grzc@ukr.net>
2018-02-13 23:43:06 +0200

    BigArray

--- src/arrayCalc/BigArray.java
+++ src/arrayCalc/BigArray.java
@@ -1,6 +1,7 @@
 package arrayCalc;
 
 import java.util.Date;
+import java.util.Scanner;
 import java.util.concurrent.*;
 
 public class BigArray {
@@ -19,36 +20,53 @@ public class BigArray {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         final int SIZE = 80_000_000;
         int[] a = bigArray(SIZE);
+
         ExecutorService service = Executors.newFixedThreadPool(2);
-
-        long d = new Date().getTime();
-
-        Callable<Double> sum = () -> {
-            double sum1 = 0;
-            for (int i = 0; i < SIZE; i++) {
-                double sin = Math.sin((double) a[i]);
-                double cos = Math.cos((double) a[i]);
-                sum1 +=sin + cos;
-            }
-            return sum1;
-        };
-
-        Future<Double> f = service.submit(sum);
-        System.out.println("Pool method:");
-        System.out.println("Result: " + f.get());
-        long sub = new Date().getTime() - d;
-        System.out.println("Time: " + sub);
-        service.shutdown();
-
-        d = new Date().getTime();
         Runnable runnable = () -> result = sinCos(a);
-        Thread thread = new Thread(runnable);
-        thread.start();
-        System.out.println("Thread method:");
-        thread.join();
-        System.out.println("Result: " + result);
+        Callable<Double> callable = () -> sinCos(a);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("N> ");
+        int n = scanner.nextInt();
+
+        Future<Double> future = service.submit(callable); //ExexuteServise submit
+        service.shutdown();
+//Pool
+        long d = new Date().getTime();
+        for (int i = 0; i < n; i++)
+            System.out.println("From future " + future.get()); //future.get
+        long sub = new Date().getTime() - d;
+        System.out.println("Time \n" + sub);
+
+//Thread
+        d = new Date().getTime();
+        for (int i = 0; i < n; i++){
+            Thread thread = new Thread(runnable);
+            thread.start();
+            thread.join();
+            System.out.println("Thread method: " + result);
+        }
         sub = new Date().getTime() - d;
         System.out.println("Time: " + sub);
+    }
+
+    private static void poolCalc(Callable<Double> sum, ExecutorService service){
+        long d = new Date().getTime();
+        Future<Double> f = service.submit(sum);
+        double result = 0;
+        try {
+            result = f.get();
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Pool method:");
+        System.out.println("Result: " + result);
+        long sub = new Date().getTime() - d;
+        System.out.println("Time: " + sub);
+//        service.shutdown();
     }
 
     private static double sinCos(int[] a){
@@ -59,15 +77,6 @@ public class BigArray {
             sum += sin + cos;
         }
         return sum;
-    }
 
-//    private static int sinCos(int[] a, int start, int finish){
-//        double sum = 0;
-//        for (int i = start; i < finish; i++) {
-//            double sin = Math.sin((double) a[i]);
-//            double cos = Math.sin((double) a[i]);
-//            sum += sin + cos;
-//        }
-//        return (int) sum;
-//    }
+    }
 }

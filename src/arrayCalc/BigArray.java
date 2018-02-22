99f3d811661e76e0d69799e05343078ccc584ddd
hhlTer <grzc@ukr.net>
2018-02-22 19:24:52 +0200

    show25 2.0

--- src/arrayCalc/BigArray.java
+++ src/arrayCalc/BigArray.java
@@ -19,44 +19,64 @@ public class BigArray {
     private static double result;
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        final int SIZE = 80_000_000;
+        final int SIZE = 8_000_000;
         int[] a = bigArray(SIZE); //fillArray
 
-        Callable<Double> callable = () -> sinCos(a);//calc in callable
+//        Callable<Double> callable = () -> sinCos(a);//calc in callable
 //Dialog
         Scanner scanner = new Scanner(System.in);
-        System.out.print("N> ");
+        System.out.print("Enter count of calculate > ");
         int n = scanner.nextInt();
+        scanner.nextLine();
+        System.out.print("Enter count of thread > ");
+        int t = scanner.nextInt();
+        scanner.close();
+
 //-----------------------------------------------------------------------------
-        ExecutorService service = Executors.newFixedThreadPool(n); //create pool
-        Future<Double> future = service.submit(callable); //submit in future
-        service.shutdown();//and shutdown
-
-//---------------------------Pool start-----------------------------------------
-        long d = new Date().getTime();
-        System.out.println("From pool: " + future.get());
-        long sub = new Date().getTime() - d;
-        System.out.println("Time \n" + sub);
-
-//-------------------------Thread start-----------------------------------------
-        d = new Date().getTime();
         for (int i = 0; i < n; i++) {
-            int start = i * a.length/n;
-            int fin = (i+1)*a.length/n;
-            int[] array = Arrays.copyOfRange(a, start, fin);//separate array
-            SumArrayThread arrayThread = new SumArrayThread(array);
-            arrayThread.start();
-            arrayThread.join();
-            result += arrayThread.getSum();
+            System.out.println("Caculate " + (i+1) + ":");
+            System.out.println("------------------------------------------------");
+            long checkPoint = new Date().getTime();
+            System.out.println("Pool result: " + calcInPool(a, t));
+            checkPoint = new Date().getTime() - checkPoint;
+            System.out.println("Time \n" + checkPoint);
+            System.out.println("------------------------------------------------");
+            checkPoint = new Date().getTime();
+            System.out.println("Thread(s) result: " + calcInTreads(a, t));
+            checkPoint = new Date().getTime() - checkPoint;
+            System.out.println("Time \n" + checkPoint);
+            System.out.println("================================================\n");
         }
-        sub = new Date().getTime() - d;
-        System.out.println("\nThread result: " + result);
-        System.out.println("Time: " + sub);
+        service.shutdown();
     }
 
-    static double sinCos(int[] a){
+    private static double sinCos(int[] a){
         double sum = 0;
         for (int anA : a) sum += Math.sin(anA) + Math.cos(anA);
         return sum;
+    }
+    private static ExecutorService service;
+
+    private static double calcInPool(int[] a, int t) throws ExecutionException, InterruptedException {
+        if (service == null) service = Executors.newFixedThreadPool(t);
+        Future<Double> future = service.submit(callable(a));
+        return future.get();
+    }
+    private static double calcInTreads(int[] a, int n) throws ExecutionException, InterruptedException {
+        double result = 0;
+        for (int i = 0; i < n; i++) {
+            int start = i * a.length / n;
+            int fin = (i + 1) * a.length / n;
+            int[] array = Arrays.copyOfRange(a, start, fin);//separate array
+            FutureTask<Double> futureTask = new FutureTask<>(callable(array));
+            new Thread(futureTask).start();
+            result += futureTask.get();
+        }
+        return result;
+    }
+
+    private static Callable<Double> callable(int[] array){
+        Callable<Double> callable = () -> sinCos(array);
+        return callable;
     }
 }
